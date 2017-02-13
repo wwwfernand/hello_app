c0f3a60b0ad7eb208754728347755dc393436987
Fernand Arioja <wwwfernand@yahoo.com>
2017-02-13 16:48:38 +0900

    finish user edit, update, index, and destroy actions

--- test/helpers/sessions_helper_test.rb
+++ test/helpers/sessions_helper_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class SessionsHelperTest < ActionView::TestCase
 
   def setup
-    @user = users(:dummy)
+    @user = users(:michael)
     remember(@user)
   end
 
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
   def setup
-    @user = users(:dummy)
+    @user = users(:michael)
   end
     
   test "login with invalid information" do
