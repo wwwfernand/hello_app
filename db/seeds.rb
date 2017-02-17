e16f47fead71c9781c7c92146e181a4822176dce
Fernand Arioja <wwwfernand@yahoo.com>
2017-02-17 15:28:07 +0900

    clean up code

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-User.create!(name:  "Example User",
+User.create!(name:  "Admin",
              email: "admin@tickleup.com",
              password:              "tickleup.com",
              password_confirmation: "tickleup.com",
@@ -13,7 +8,7 @@ User.create!(name:  "Example User",
              activated: true,
              activated_at: Time.zone.now)
              
-User.create!(name:  "Example User",
+User.create!(name:  "Guest",
              email: "guest@tickleup.com",
              password:              "tickleup.com",
              password_confirmation: "tickleup.com",
@@ -23,7 +18,7 @@ User.create!(name:  "Example User",
 
 99.times do |n|
   name  = Faker::Name.name
-  email = "example-#{n+1}@tickleup.com"
+  email = "user-#{n+1}@tickleup.com"
   password = "password"
   User.create!(name:  name,
               email: email,
