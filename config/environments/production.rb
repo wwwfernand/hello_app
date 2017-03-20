2ae930bf02e7b73c68e9256442d4b022e67cd7de
Fernand M. Arioja <wwwfernand@yahoo.com>
2017-03-20 15:20:57 +0900

    update mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
     config.action_mailer.delivery_method = :smtp
-    host = 'template-tickleup.herokuapp.com'
+    host = ENV['HOST_HEROKUAPP'],
     config.action_mailer.default_url_options = { host: host }
     ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
